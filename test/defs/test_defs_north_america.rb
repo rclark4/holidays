2f0bdfa1dc029179b3d9734b74a37c496d6de3b3
Ryan Clark <itsryanclark@gmail.com>
2014-01-07 15:45:38 -0600

    Fix syntax error

--- test/defs/test_defs_north_america.rb
+++ test/defs/test_defs_north_america.rb
@@ -32,8 +32,8 @@ end
 end
 
 
-{Date.civil(2007,1,1) => 'Año nuevo', 
- Date.civil(2007,2,5) => 'Día de la Constitución', 
+{Date.civil(2007,1,1) => 'Año nuevo',
+ Date.civil(2007,2,5) => 'Día de la Constitución',
  Date.civil(2007,5,1) => 'Día del Trabajo',
  Date.civil(2007,5,5) => 'Cinco de Mayo',
  Date.civil(2007,9,16) => 'Día de la Independencia',
@@ -42,9 +42,9 @@ end
  Date.civil(2007,11,19) => 'Día de la Revolución',
  Date.civil(2007,12,25) => 'Navidad'}.each do |date, name|
   assert_equal name, (Holidays.on(date, :mx, :informal)[0] || {})[:name]
-end  
+end
 
-{Date.civil(2008,1,1) => 'New Year\'s Day', 
+{Date.civil(2008,1,1) => 'New Year\'s Day',
  Date.civil(2008,1,21) => 'Martin Luther King, Jr. Day',
  Date.civil(2008,2,18) => 'Presidents\' Day',
  Date.civil(2008,5,26) => 'Memorial Day',
@@ -54,7 +54,7 @@ end
  Date.civil(2008,11,11) => 'Veterans Day',
  Date.civil(2008,11,27) => 'Thanksgiving',
  Date.civil(2008,11,28) => 'Day after Thanksgiving',
- Date.civil(2008,12,24) => 'Christmas Eve'
+ Date.civil(2008,12,24) => 'Christmas Eve',
  Date.civil(2008,12,25) => 'Christmas Day'}.each do |date, name|
   assert_equal name, (Holidays.on(date, :us)[0] || {})[:name]
 end
